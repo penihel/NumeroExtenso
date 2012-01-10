77c66c8076b45b7ba1c00d086c647b6d65dff991
Penihel Roosewelt <penihel@gmail.com>
2012-01-10 20:42:03 -0200

    Commit inicial

--- src/NumeroExtenso/NumeroExtenso/Numero.cs
+++ src/NumeroExtenso/NumeroExtenso/Numero.cs
@@ -5,7 +5,23 @@ using System.Text;
 
 namespace NumeroExtenso
 {
-    class Numero
+    /// <summary>
+    /// Representa um número com seus digitos
+    /// </summary>
+    internal class Numero
     {
+        /// <summary>
+        /// Valor Decimal do Número
+        /// </summary>
+        private decimal _valor;
+
+        /// <summary>
+        /// Cria um número que será passado para extenso
+        /// </summary>
+        /// <param name="v">Valor decimal do número</param>
+        public Numero(decimal v)
+        {
+            this._valor = v;
+        }
     }
 }
